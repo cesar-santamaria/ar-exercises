19ac25f6aa6d4715239907399104213631a5a27e
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-06-01 10:10:47 -0500

    add calculations to determine average annual revenue for all stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,22 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
+=begin 
+Output the total revenue for the entire company (all stores), using Active Record's .sum calculation method.
+On the next line, also output the average annual revenue for all stores.
+Output the number of stores that are generating $1M or more in annual sales. Hint: Chain together where and size (or count) Active Record methods.
+=end
+
 # Your code goes here ...
+
+total_revenue = Store.sum(:annual_revenue)
+
+total_stores = Store.count
+
+average_revenue= total_revenue/total_stores
+
+pp Store.where('annual_revenue > 1000000')
+
+rich_stores = Store.where('annual_revenue > 1000000').count
+
+puts rich_stores
